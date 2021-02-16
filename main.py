f72775eecc9aa913de0a5150e977055e0f13cf93
LucasCross2023 <817750@mystma.org>
2021-02-16 15:54:11 +0000

    yankees try 2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def yankees(x):
-    return years[x-1]  
+    years = (1923, 1927, 1943)
+
+    return years[x-1]
     
-
-    years = [1923, 1927, 1932]
-
-    print(yankees(1))
+    
+    
+print(yankees(1))
